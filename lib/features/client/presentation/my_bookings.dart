ca423d71ed565593fce9e662f29b937076451968
mhmedHas <mhmed.hassan.antaka@gmail.com>
2026-09-19 12:05:26 +0300

    Fix bookings screen widget duplication

--- lib/features/client/presentation/my_bookings.dart
+++ lib/features/client/presentation/my_bookings.dart
@@ -347,31 +347,6 @@ class _MyBookingsScreenState extends State<MyBookingsScreen>
     }
   }
 
-class _StatusBadge extends StatelessWidget {
-  final String label;
-  final Color color;
-
-  const _StatusBadge({required this.label, required this.color});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 5),
-      decoration: BoxDecoration(
-        color: color.withValues(alpha: 0.12),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(
-        label,
-        style: TextStyle(
-          color: color,
-          fontSize: 11,
-          fontWeight: FontWeight.w700,
-        ),
-      ),
-    );
-  }
-}
 class _InfoItem extends StatelessWidget {
   final IconData icon;
   final String title;
